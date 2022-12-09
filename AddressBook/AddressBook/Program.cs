fb25ef5e9f131fb5e73d04e77f8513ba06854824
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-09 09:05:19 +0530

    [Ajay]- Adding contact in address book

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Addressbook addressbook = new Addressbook();
+            addressbook.CreatContact();
+
+        }
+    }
+}
